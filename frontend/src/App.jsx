b89c346f11dde6c2502cd71abed0a2cb64a9bc11
KoKaTiM1 <progamer0001001001@gmail.com>
2025-05-08 12:21:11 +0300

    Updat

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,6 @@
 // frontend/src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AdminPanel from './pages/AdminPanel';
-import Login from './pages/Login';
-import VolunteerMap from './pages/VolunteerMap';
 import './styles/App.css';
 import HomeScreen from './pages/HomeScreen';
 import ReportPage from './pages/ReportPage'; // נוספה שורת ייבוא לעמוד הדיווח
@@ -20,15 +17,10 @@ function App() {
             <Route path="/report" element={<ReportPage />} /> {/* נוספה שורת הנתיב */}
           </Routes>
         </div>
-      <div className="p-4">
-        <Routes>
-          <Route path="/" element={<VolunteerMap />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/admin" element={<AdminPanel />} />
-        </Routes>
       </div>
     </Router>
   );
 }
+
 
 export default App;
